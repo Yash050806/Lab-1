99825c99ca00e489b5bff572d99e36f1370eca64
unknown <yashingale0508@gmail.com>
2025-05-08 19:40:47 -0400

    new main

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,42 +3,17 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main(int t) {
 
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
             iFuzzBizz(i);
         }
-//        Integer i;
-//        i = 0;
-//        while (i < 100) {
-//            boolean divisibleBy3 = i % 3 == 0;
-//            boolean divisibleBy5 = i % 5 == 0;
-//
-//            // Print our appropriate result.
-//            if (divisibleBy3 && divisibleBy5) {
-//
-//                System.out.println("Fizz Buzz");
-//
-//            } else if (divisibleBy3) {
-//
-//                System.out.println("Fizz");
-//
-//            } else if (divisibleBy5) {
-//
-//                System.out.println("Buzz");
-//
-//            } else {
-//
-//                System.out.println(i);
-//            }
-//            i++;
-//        }
-//    }
+
 }
 
-    private static void iFuzzBizz(int i) {
+    public static void iFuzzBizz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
